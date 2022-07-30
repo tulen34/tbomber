60568a7e199362a2da701c8df199269772d430ed
Ilya <69756484+tulenchick@users.noreply.github.com>
2022-07-30 20:57:06 +0300

    Add `random` funcs

--- tbomber/utils.py
+++ tbomber/utils.py
@@ -1,26 +1,49 @@
 from httpx import AsyncClient
-from typing import Literal
+import re
+import random
 
 client = AsyncClient()
 
 
 class PhoneNumber:
     def __init__(self, phone_number: str):
-        ...
-
-    def format(self, mask: str) -> str:
-        ...
-
-    @property
-    def country_code(self):
-        raise NotImplemented()
+        if not re.match(r'\d{11}', phone_number):
+            raise ValueError(phone_number)
+        self.value = phone_number
 
     def __repr__(self) -> str:
-        ...
+        return self.value
+
+    def format(self, mask: str, mask_char='*') -> str:
+        formatted = ''
+        i = 0
+        for c in mask:
+            if c == mask_char:
+                formatted += self.value[i]
+                i += 1
+            else:
+                formatted += c
+        return formatted
 
 
-def generate_info(category: Literal['russian_word',
-                                    'user_name',
-                                    'email']) -> str:
-    ...
+def __unicode_range(l, r):
+    return [chr(i) for i in range(ord(l), ord(r) + 1)]
 
+
+def __random_str(sources):
+    length = random.randint(7, 12)
+    return ''.join(random.choices(sources, k=length))
+
+
+def random_russian_word() -> str:
+    return __random_str(__unicode_range('А', 'я'))
+
+
+def random_english_word() -> str:
+    return __random_str([*__unicode_range('a', 'z'),
+                         *__unicode_range('A', 'Z')])
+
+
+def random_email() -> str:
+    random_domain = random.choice(['gmail.com', 'mail.ru', 'yandex.ru'])
+    return '{}@{}'.format(random_english_word(), random_domain)
