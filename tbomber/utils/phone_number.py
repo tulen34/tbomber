95af09d69c85d7a7848980a054d472fd347dff83
Ilya <69756484+tulenchick@users.noreply.github.com>
2022-08-01 19:39:55 +0300

    Add new methods & update `__init__`

--- tbomber/utils/phone_number.py
+++ tbomber/utils/phone_number.py
@@ -1,22 +1,30 @@
 import re
+import string
+from typing import AnyStr
 
 
 class PhoneNumber:
-    def __init__(self, phone_number: str):
-        if not re.match(r'\d{11}', phone_number):
+    def __init__(self, phone_number: AnyStr):
+        self.__value = ''.join(c for c in phone_number if c in string.digits)
+        if len(self.__value) != 11:
             raise ValueError(phone_number)
-        self.value = phone_number
 
     def __str__(self) -> str:
-        return self.value
+        return self.__value
+
+    def __mod__(self, mask: str) -> str:
+        return self.format(mask)
 
     def format(self, mask: str, mask_char='*') -> str:
         formatted = ''
         i = 0
         for c in mask:
             if c == mask_char:
-                formatted += self.value[i]
+                formatted += self.__value[i]
                 i += 1
             else:
                 formatted += c
         return formatted
+
+    def with_plus(self) -> str:
+        return '+' + str(self)
